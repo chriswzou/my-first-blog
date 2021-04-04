bddd4b65fec2d11e30a91f80f7347d576fc2df6e
Christopher Zou <cwzou@berkeley.edu>
2021-04-04 00:07:26 -0700

    Added view and template for blog post and CSS for site

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+# import requests
+# import json
+
+# api_key = 'BQwlCAeXaR55mC-EAmnEHBt6r7VD32-Bev9pPHvDN7tHLykQXG0I6RMPIBx4ShNBb7G0eILSzqlWa2iHhw2KVEzCqvyhxgdv3agR_5haNWyftS_j85dzsmCfnGFpYHYx'
+# headers = {
+#     'Authorization': 'Bearer %s' % api_key
+# }
+
+
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 # Create your views here.
